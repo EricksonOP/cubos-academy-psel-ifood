954fe75e0bd45d0ae53398c1b5c867ddafd6e222
Erickson <erickoparaguassu@hotmail.com>
2021-08-10 20:24:26 -0300

    App para ajudar na campanha de vacinação

---
+++ Vacinacao.js
@@ -0,0 +1,15 @@
+function solucao(obj) {
+	//seu codigo aqui
+    if (obj.jaTomouVacina === true){
+        console.log("JA TOMOU VACINA");
+        
+    } else if (obj.idade >= 60) {
+        console.log("APTA POR IDADE");
+    } else if (obj.temComorbidade === true){
+        console.log ("APTA POR COMORBIDADE");
+    }  else {
+        console.log("INAPTA")
+    }
+               
+        
+}
