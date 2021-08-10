76f361e3ac2aff168e0cdabd967e14046d7704ac
Erickson <erickoparaguassu@hotmail.com>
2021-08-10 20:22:21 -0300

    App para ajudar seu irmao no esconde esconde, que so sabe contar ate 20

---
+++ EscondeEsconde.js
@@ -0,0 +1,13 @@
+function solucao(numero) {
+	//seu codigo aqui
+    let i = 21
+    if (numero <= 20){
+        console.log("VOCE CONSEGUE, MANINHO");
+    } else { while (i <= numero) {
+        console.log(i)
+        i ++
+    }     
+        
+        
+    }
+}
